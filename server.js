10bee493a3800aa6ef1355bc675839ac887eb99a
Jakub Nowakowski <contact@jnowakowski.com>
2018-03-27 15:50:46 +0200

    Logging timestamp

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ const APP_PORT     = 80;
 
 const hostname = os.hostname();
 
+// Add timestamp to log output
+require('console-stamp')(console, '[HH:MM:ss.l]');
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
+    console.log('Received request')
     res.render('index', { hostname: hostname })
 })
 
 app.listen(APP_PORT)
 
-console.log('Web server started on port: ' + APP_PORT);
+console.log('Web server started on host: ' + hostname + ', port: ' + APP_PORT);
