351fcfb734ad5ab247f32316a2649de9c96dd61c
Jakub Nowakowski <contact@jnowakowski.com>
2018-03-28 11:54:43 +0200

    Environment variables handling

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ const version   = process.env.npm_package_version
 // Add timestamp to log output
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
+// Discover Environment Variables
+var vars = []
+if(process.env.ENV_VAR1) vars.push(process.env.ENV_VAR1)
+if(process.env.ENV_VAR2) vars.push(process.env.ENV_VAR2)
+
+console.log("Discovered environment variables: " + vars)
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     console.log('Received request for path: ' + req.path)
-    res.render('index', { hostname: hostname, version: version})
+    res.render('index', { hostname: hostname, version: version, vars: vars})
 })
 
 app.get('/version', function (req, res) {
